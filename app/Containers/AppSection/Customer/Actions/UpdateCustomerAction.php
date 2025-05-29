9732dfd869f0363cea8e0aed5b971a9eff2e3c31
yahluo-dev <yarikvas2003@gmail.com>
2025-05-29 14:24:34 +0200

    feat(UpdateCustomer): Add Route, Request, Action, Task

---
+++ app/Containers/AppSection/Customer/Actions/UpdateCustomerAction.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Containers\AppSection\Customer\Actions;
+
+use App\Containers\AppSection\Customer\Models\Customer;
+use App\Containers\AppSection\Customer\Tasks\UpdateCustomerTask;
+use App\Containers\AppSection\Customer\UI\API\Requests\UpdateCustomerRequest;
+use App\Ship\Parents\Actions\Action as ParentAction;
+
+final class UpdateCustomerAction extends ParentAction
+{
+    public function __construct(
+        private readonly UpdateCustomerTask $updateCustomerTask,
+    ) {
+    }
+
+    public function run(UpdateCustomerRequest $request): Customer
+    {
+        $data = $request->sanitize([
+            // add your request data here
+        ]);
+
+        return $this->updateCustomerTask->run($data, $request->id);
+    }
+}
---
+++ app/Containers/AppSection/Customer/UI/API/Requests/UpdateCustomerRequest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Containers\AppSection\Customer\UI\API\Requests;
+
+use App\Ship\Parents\Requests\Request as ParentRequest;
+
+final class UpdateCustomerRequest extends ParentRequest
+{
+    protected array $decode = [];
+
+    public function rules(): array
+    {
+        return [];
+    }
+}
