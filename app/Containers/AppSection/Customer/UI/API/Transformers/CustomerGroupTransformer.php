93bfc4091f43e2acedaca9bbca9f429653989d40
yahluo-dev <yarikvas2003@gmail.com>
2025-06-02 10:56:06 +0200

    feat(CustomerGroupTransformer, CustomerTransformer): Add groups and customers as available includes

--- app/Containers/AppSection/Customer/UI/API/Transformers/CustomerGroupTransformer.php
+++ app/Containers/AppSection/Customer/UI/API/Transformers/CustomerGroupTransformer.php
@@ -4,12 +4,15 @@ namespace App\Containers\AppSection\Customer\UI\API\Transformers;
 
 use App\Containers\AppSection\Customer\Models\CustomerGroup;
 use App\Ship\Parents\Transformers\Transformer as ParentTransformer;
+use Apiato\Http\Resources\Collection;
 
 final class CustomerGroupTransformer extends ParentTransformer
 {
     protected array $defaultIncludes = [];
 
-    protected array $availableIncludes = [];
+    protected array $availableIncludes = [
+        'customers'
+    ];
 
     public function transform(CustomerGroup $customergroup): array
     {
@@ -19,10 +22,13 @@ final class CustomerGroupTransformer extends ParentTransformer
             'name' => $customergroup->name,
             'created_at' => $customergroup->created_at,
             'updated_at' => $customergroup->updated_at,
-            'created_at' => $customergroup->created_at,
-            'updated_at' => $customergroup->updated_at,
             'readable_created_at' => $customergroup->created_at->diffForHumans(),
             'readable_updated_at' => $customergroup->updated_at->diffForHumans(),
         ];
+    }
+
+    public function includeCustomers(CustomerGroup $customer_group): Collection
+    {
+        return $this->collection($customer_group->customers, new CustomerTransformer());
     }
 }
--- app/Containers/AppSection/Customer/UI/API/Transformers/CustomerTransformer.php
+++ app/Containers/AppSection/Customer/UI/API/Transformers/CustomerTransformer.php
@@ -4,12 +4,15 @@ namespace App\Containers\AppSection\Customer\UI\API\Transformers;
 
 use App\Containers\AppSection\Customer\Models\Customer;
 use App\Ship\Parents\Transformers\Transformer as ParentTransformer;
+use Apiato\Http\Resources\Collection;
 
 final class CustomerTransformer extends ParentTransformer
 {
     protected array $defaultIncludes = [];
 
-    protected array $availableIncludes = [];
+    protected array $availableIncludes = [
+        'customer_groups'
+    ];
 
     public function transform(Customer $customer): array
     {
@@ -18,11 +21,15 @@ final class CustomerTransformer extends ParentTransformer
             'id' => $customer->getHashedKey(),
             'name' => $customer->name,
             'email' => $customer->email,
-            'customer_groups' => $customer->customer_groups,
             'created_at' => $customer->created_at,
             'updated_at' => $customer->updated_at,
             'readable_created_at' => $customer->created_at->diffForHumans(),
             'readable_updated_at' => $customer->updated_at->diffForHumans(),
         ];
     }
+    public function includeCustomerGroups(Customer $customer): Collection
+    {
+        return $this->collection($customer->customer_groups, new CustomerGroupTransformer());
+    }
+
 }
